6d0d4559cfb71eeb459663ef270fd3e7282f216b
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2021-09-11 17:00:55 +0200

    stringi

---
+++ Chapter3/Chapter3/Source.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::size_t;
+
+int main() {
+	//3.11
+		//const string s = "Keep out!";
+		//for (auto& c : s) {}
+	//3.10
+		//string ss = "HELLO, THERE!!! CAN you hear me??.";
+		//for (auto s : ss) {
+		//	if(!ispunct(s)){
+		//		cout << s;
+		//	}
+		//}
+	// 3.8b
+		//string ss("slowo");
+		//cout << ss;
+		//for (size_t it = 0; it < ss.size(); it++){
+		//	ss[it] = 'x';
+		//}
+		//cout << endl;
+		//cout << ss;
+
+	// 3.8a
+		//string ss("slowo");
+		//cout << ss;
+		//size_t it = 0;
+		//while (it < ss.size()) {
+		//	ss[it] = 'x';
+		//	it++;
+		//}
+		//cout << endl;
+		//cout << ss;
+
+	// 3.7
+		//string ss("slowo");
+		//cout << ss;
+		//for (char & s : ss) {
+		//	s = 'x';
+		//}
+		//cout << endl;
+		//cout << ss;
+
+	// 3.6
+		//string ss("slowo");
+		//cout << ss;
+		//for (auto& s : ss) {
+		//	s = 'x';
+		//}
+		//cout << endl;
+		//cout << ss;
+
+}
